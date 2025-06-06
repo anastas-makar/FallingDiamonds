0d74d249f64ab46a97b93943282eb3cc3804b22d
Anastasiia Makarenko <irishmoth@yandex.ru>
2025-06-06 19:59:45 +0300

    Правлю настройки для gradle

---
+++ app/build.gradle.kts
@@ -0,0 +1,88 @@
+import java.util.Properties
+import java.io.FileInputStream
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+    id("com.google.devtools.ksp")
+}
+
+android {
+    namespace = "pro.progr.fallingdiamonds"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+        targetSdk = 34
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        version = "0.0.1-alpha"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+
+        }
+        debug {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.2"
+    }
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+dependencies {
+    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.ui:ui-tooling:1.5.4")
+    implementation("androidx.compose.material:material:1.5.4")
+    implementation("androidx.compose.foundation:foundation:1.5.4")
+    implementation("androidx.activity:activity-compose:1.8.0")
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            artifact("$buildDir/outputs/aar/app-release.aar")
+
+            groupId = "pro.progr"
+            artifactId = "fallingdiamonds"
+            version = "0.0.1-alpha"
+        }
+    }
+    repositories {
+        maven {
+            url = uri("file://${buildDir}/repo")
+        }
+    }
+}
