a2b3e3a6d5be4333717c1dd4eb34344cebc2b652
Anastasiia Makarenko <irishmoth@yandex.ru>
2025-06-08 23:21:58 +0300

    Подключаю DiamondApi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,9 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    implementation("pro.progr:diamond-api:0.0.1-alpha")
+
+
 }
 
 afterEvaluate {
