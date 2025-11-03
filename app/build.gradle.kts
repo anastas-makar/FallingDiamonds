3eead115ab4c525dfac4a326756debfc3abf6e68
Anastasiia Makarenko <irishmoth@yandex.ru>
2025-11-03 15:44:05 +0300

    обновляю diamond-api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,7 +73,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("pro.progr:diamond-api:1.0.0-alpha")
+    implementation("pro.progr:diamond-api:1.0.1-alpha")
 
 }
 
